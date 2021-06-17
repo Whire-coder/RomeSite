c4f97d8ae173ad96a0e45eb4945722544219d1ae
Whire-coder <romeo.pst44@gmail.com>
2021-06-17 10:09:52 +0200

    ⚙️ Add config file

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const express = require("express");
-const router = require("./router");
 const fetch = require("node-fetch");
 const path = require("path");
 
-const config = {
-    ip: "45.90.162.185",
-    port: "80",
-    https: true,
-    name: "rome-dev.fr"
-};
+const router = require("./router");
+const config = require("./config.json");
 
 const app = express();
 
@@ -19,6 +14,7 @@ app.use(express.static(__dirname + '/public'));
 app.set("views", path.join(__dirname, "/views"));
 
 app.use(async function(req, res, next){
+
     await fetch("http://45.90.162.185:81/web", {
         headers: { "Authorization": "Bearer mouchou" }
     })
@@ -28,10 +24,11 @@ app.use(async function(req, res, next){
             serverCount: json.serverCount,
             userCount: json.userCount
         };
-    })
-
+    });
     next();
+
 });
+
 app.use(router);
 
 app.listen(config.port, config.ip, () => {
