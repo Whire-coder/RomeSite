4ee977357a718eb3cbdb65788154858a046f128c
Whire-coder <romeo.pst44@gmail.com>
2021-10-16 12:28:20 +0200

    🎟️ Fix a bug with the login system

--- public/js/login.js
+++ public/js/login.js
@@ -1,26 +1,38 @@
 function getNameWithToken() {
+
     fetch('https://discord.com/api/users/@me', {
         headers: {
             authorization: `Bearer ${localStorage.getItem("token")}`,
         },
     })
-        .then(result => result.json())
-        .then(response => {
-            const { username, discriminator } = response;
-            const loginLi = document.getElementById('login');
+    .then(result => result.json())
+    .then(response => {
+        const { username, discriminator } = response;
 
-            loginLi.innerText = `${username}#${discriminator}`;
-            loginLi.removeAttribute("href");
-        })
-        .catch(console.error);
-}
+        if(!username || !discriminator) {
+            localStorage.removeItem("token");
+            return;
+        };
+
+        return `${username}#${discriminator}`;
+    })
+    .catch(error => {
+        localStorage.removeItem("token");
+        return;
+    });
+};
 
 window.onload = () => {
 
-    if(localStorage.getItem("token")) getNameWithToken()
-    else {
+    if(localStorage.getItem("token")) {
+        const nameAndTag = getNameWithToken();
+        const loginLi = document.getElementById('login');
+
+        loginLi.innerText = nameAndTag;
+        loginLi.removeAttribute("href");
+    } else {
         const fragment = new URLSearchParams(window.location.hash.slice(1));
-        const token = new URLSearchParams(window.location.hash.slice(1)).get("access_token");
+        const token = fragment.get("access_token");
 
         if(!token) return;
 
