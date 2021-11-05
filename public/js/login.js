3b69c6853c9dcacd542841fea989aab4323d5026
Whire-coder <romeo.pst44@gmail.com>
2021-11-05 17:03:45 +0100

    🎟️ Fix a login bug

--- public/js/login.js
+++ public/js/login.js
@@ -8,35 +8,40 @@ function getNameWithToken() {
     .then(result => result.json())
     .then(response => {
         const { username, discriminator } = response;
-
+        console.log(response)
+    
         if(!username || !discriminator) {
             localStorage.removeItem("token");
             return;
         };
-
-        return `${username}#${discriminator}`;
+    
+        const nameAndTag = `${username}#${discriminator}`;
+        const loginLi = document.getElementById('login');
+    
+        loginLi.innerText = nameAndTag;
+        loginLi.removeAttribute("href");
     })
     .catch(error => {
         localStorage.removeItem("token");
         return;
     });
 };
-
+    
 window.onload = () => {
-
+    
     if(localStorage.getItem("token")) {
         const nameAndTag = getNameWithToken();
         const loginLi = document.getElementById('login');
-
+    
         loginLi.innerText = nameAndTag;
         loginLi.removeAttribute("href");
     } else {
         const fragment = new URLSearchParams(window.location.hash.slice(1));
         const token = fragment.get("access_token");
-
+    
         if(!token) return;
-
+    
         localStorage.setItem("token", token);
-        getNameWithToken()
+        getNameWithToken
     }
 };
